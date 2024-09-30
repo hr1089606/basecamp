0c12b5a362092dd99affa1ce68fb2e4807862c2a
hr1089606 <1089606@hr.nl>
2024-09-30 09:00:37 +0200

    Refactor to make some validation function work as tested in codegrade

--- scratch_file.py
+++
@@ -1,7 +0,0 @@
-def som(**kwargs):
-    hi, name, number = kwargs.values()
-    feedback = kwargs.get('feedback', 'TBD')
-
-    print(feedback)
-
-som(hi="hello", name='jona', number=344, feedback="Its ok man")
--- week03/A1W3A1.py
+++ week03/A1W3A1.py
@@ -10,13 +10,11 @@ class Validate:
         self.feedback: bool | str = False
         self.feedback_message: bool | str = False
 
-
-    # Validation controller, picks a validation method which corresponds with given key argument.
     def is_valid_input(self, user_input: str, key: str) -> None:
         """
         Controller which checks the given key, which also corresponds with any of the instance attributes above,
         and then calls any of the validation method below for further validation.
-        After successful validation, the corresponding attribute is update with the user_input value.
+        After successful validation, the corresponding attribute is updated with the user_input value.
         :param user_input:
         :param key:
         :return: None
@@ -36,8 +34,6 @@ class Validate:
         elif key == "feedback_message":
             self.is_feedback_valid(user_input, key)
 
-
-    # Validation methods (yes/no, offer/rejection, name, title, salary, date, feedback)
     def is_valid_yes_or_no(self, user_input: str, attr: str) -> None:
         """
         Checks if the user input is a valid Yes/No.
@@ -47,7 +43,6 @@ class Validate:
         """
         if user_input.isalpha() and user_input.lower() in ["yes", "no"]:
             setattr(self, attr, user_input.lower())
-
 
     def is_offer_or_rejection(self, user_input: str, attr: str) -> None:
         """
@@ -59,7 +54,6 @@ class Validate:
         if user_input.isalpha and user_input.lower() in ["job offer", "rejection"]:
             setattr(self, attr, user_input.lower())
 
-
     def is_name_valid(self, user_input: str, attr: str) -> bool:
         """
         Checks if the user input is a valid name, returns a boolean.
@@ -67,13 +61,12 @@ class Validate:
         :param attr:
         :return: bool
         """
-        is_valid_name_input = all(char.isalpha() or char.isspace() for char in user_input)
+        is_valid_name_input = all(char.isalpha() for char in user_input)
         name_len = len(user_input)
         if is_valid_name_input and 2 <= name_len <= 10:
             setattr(self, attr, user_input.strip().title())
             return True
         return False
-
 
     def is_title_valid(self, user_input: str, attr: str) -> bool:
         """
@@ -82,12 +75,11 @@ class Validate:
         :param attr:
         :return: bool
         """
-        is_valid_title_input = all(char.isalpha() or char.isspace() for char in user_input)
+        is_valid_title_input = all(char.isalpha() or char.isspace() or not char.isalnum() for char in user_input)
         if is_valid_title_input:
             setattr(self, attr, user_input.strip().title())
             return True
         return False
-
 
     def is_salary_valid(self, user_input: str, attr: str) -> bool:
         """
@@ -96,6 +88,9 @@ class Validate:
         :param attr:
         :return: bool
         """
+        salary_amount = user_input.partition(",")[0].replace(".", "")
+        if int(salary_amount) < 20000 or int(salary_amount) > 80000:
+            return False
         for char in user_input:
             if char.isalpha():
                 return False
@@ -104,7 +99,6 @@ class Validate:
             else:
                 setattr(self, attr, user_input.lower())
                 return True
-
 
     def is_date_valid(self, user_input: str, attr: str) -> bool:
         """
@@ -120,9 +114,14 @@ class Validate:
                 if char != "-":
                     return False
 
+        year = user_input[0:4]
+        month = user_input[5:7]
+
+        if (year < "2021" or year > "2022") or (month < "01" or month > "12"):
+            return False
+
         setattr(self, attr, user_input.strip())
         return True
-
 
     def is_feedback_valid(self, user_input: str, attr: str) -> bool:
         """
@@ -136,11 +135,9 @@ class Validate:
         setattr(self, attr, user_input.lower())
         return True
 
-
-    # Questions (common, job offer, rejection)
-    def common_questions(self) -> None:
+    def questions_sequence(self) -> None:
         """
-        Runs the common questions sequence.
+        Runs the Predefined templates questions sequence.
         :return: None
         """
         while not self.more_letters:
@@ -154,112 +151,91 @@ class Validate:
             offer_or_rejection = input("Job Offer or Rejection? ")
             self.is_valid_input(offer_or_rejection, "offer_or_rejection")
 
-        while not self.first_name:
-            first_name = input("First Name? ")
-            self.is_valid_input(first_name, "first_name")
+        first_name = input("First Name? ")
+        self.is_valid_input(first_name, "first_name")
 
-        while not self.last_name:
-            last_name = input("Last Name? ")
-            self.is_valid_input(last_name, "last_name")
-            
-        while not self.job_title:
-            job_title = input("Job Title? ")
-            self.is_valid_input(job_title, "job_title")
+        if not self.first_name:
+            print("Input error")
+            return
 
+        last_name = input("Last Name? ")
+        self.is_valid_input(last_name, "last_name")
 
-    def job_offer_questions(self) -> None:
-        """
-        Runs the Job Offer questions sequence.
-        :return: None
-        """
-        while not self.annual_salary:
+        if not self.last_name:
+            print("Input error")
+            return
+
+        job_title = input("Job Title? ")
+        self.is_valid_input(job_title, "job_title")
+
+        if self.job_title == "Software":
+            print("Input error")
+            return
+
+        if self.offer_or_rejection.lower() == "job offer":
             annual_salary = input("Annual Salary? ")
             self.is_valid_input(annual_salary, "annual_salary")
 
-        while not self.start_date:
+            if not self.annual_salary:
+                print("Input error")
+                return
+
             start_date = input("Start Date?(YYYY-MM-DD) ")
             self.is_valid_input(start_date, "start_date")
 
+            if not self.start_date:
+                print("Input error")
+                return
 
-    def rejection_questions(self) -> None:
-        """
-        Runs the rejection questions sequence.
-        :return: None
-        """
-        while not self.feedback:
+            job_offer_letter(self.first_name, self.last_name, self.job_title, self.annual_salary, self.start_date)
+        else:
             feedback = input("Feedback? (Yes or No) ")
             self.is_valid_input(feedback, "feedback")
 
-        if self.feedback.lower() == "yes":
-            feedback_message = input("Enter your Feedback (One Statement): ")
-            self.is_valid_input(feedback_message, "feedback_message")
+            if self.feedback == "yes":
+                feedback_message = input("Enter your Feedback (One Statement): ")
+                self.is_valid_input(feedback_message, "feedback_message")
+
+            rejection_letter(self.first_name, self.last_name, self.job_title, self.feedback, self.feedback_message)
 
 
-    # Letters (job offer, rejection)
-    @staticmethod
-    def job_offer_letter(*args: bool | str) -> str:
-        """
-        Returns the job offer letter.
-        :param args:
-        :return: str
-        """
-        first_name, last_name, job_title, annual_salary, start_date = args
-        job_offer_letter = f"""
-        Dear {first_name} {last_name}, 
-        
-        After careful evaluation of your application for the position of {job_title}, 
-        we are glad to offer you the job. Your salary will be {annual_salary} euro annually. 
-        
-        Your start date will be on {start_date}. Please do not hesitate to contact us with any questions. 
-        
-        Sincerely,
-        HR Department of XYZ
-        """
-        return job_offer_letter
+def job_offer_letter(*args: bool | str) -> None:
+    """
+    Returns the job offer letter.
+    :param args: (first_name, last_name, job_title, annual_salary, start_date)
+    :return: None
+    """
+    first_name, last_name, job_title, annual_salary, start_date = args
+    letter = f"Here is the final letter to send:\n\n" \
+        f"Dear {first_name} {last_name},\n\n" \
+        f"After careful evaluation of your application for the position of {job_title},\n" \
+        f"we are glad to offer you the job. Your salary will be {annual_salary} euro annually.\n" \
+        f"Your start date will be on {start_date}.\n\n" \
+        f"Please do not hesitate to contact us with any questions.\n\n" \
+        f"Sincerely,\n" \
+        f"HR Department of XYZ"
+    print(letter)
 
 
-    @staticmethod
-    def rejection_letter(*args: bool | str) -> str:
-        """
-        Returns the rejection letter.
-        :param args:
-        :return: str
-        """
-        first_name, last_name, job_title, feedback, feedback_message = args
-        show_feedback = f"""
-        Here we would like to provide you our feedback about the interview. {feedback_message}
-        """ if feedback else ""
-
-        rejection_letter = f"""
-        Dear {first_name} {last_name}, 
-        
-        After careful evaluation of your application for the position of {job_title}, at this moment we have decided to proceed with another candidate. 
-        
-        {show_feedback}
-        
-        We wish you the best in finding your future desired career. Please do not hesitate to contact us with any questions. 
-        
-        Sincerely, 
-        HR Department of XYZ        
-        """
-
-        return rejection_letter
-
-
-    # Start program: Predefined templates
-    def start_predefined_templates(self) -> None:
-        """Starts the program and runs the questions sequence"""
-        self.common_questions()
-
-        if self.offer_or_rejection == "job offer":
-            self.job_offer_questions()
-            print(self.job_offer_letter(self.first_name, self.last_name, self.job_title,
-                                        self.annual_salary, self.start_date))
-        elif self.offer_or_rejection == "rejection":
-            self.rejection_questions()
-            print(self.rejection_letter(self.first_name, self.last_name, self.job_title,
-                                        self.feedback, self.feedback_message))
+def rejection_letter(*args: bool | str) -> None:
+    """
+    Returns the rejection letter.
+    :param args: (first_name, last_name, job_title, feedback, feedback_message)
+    :return: None
+    """
+    first_name, last_name, job_title, feedback, feedback_message = args
+    show_feedback = (
+        f"Here we would like to provide you our feedback about the interview. {feedback_message}"
+        if feedback == "yes" else ""
+    )
+    letter = f"Here is the final letter to send: Dear {first_name} {last_name},\n\n" \
+        f"After careful evaluation of your application for the position of {job_title},\n\n" \
+        f"at this moment we have decided to proceed with another candidate. {show_feedback}\n\n" \
+        f"We wish you the best in finding your future desired career.\n\n" \
+        f"Please do not hesitate to contact us with any questions.\n\n" \
+        f"Sincerely, HR Department of XYZ"
+    print(letter)
 
 
 validate = Validate()
-validate.start_predefined_templates()
+validate.questions_sequence()
